c4bea7daf311ab5f7f23235465c83d9808b79de7
“Kgotso-Koete” <kgkoete@gmail.com>
2019-07-27 22:23:51 +0200

    Add all course files

---
+++ 5. Responsive Website Tutorial and Examples/1. My projects/Week 2/Music-Machine-master/server/main.js
@@ -0,0 +1,7 @@
+import '../imports/api/musicMachine.js';
+
+// MusicMachine.remove({});
+if (MusicMachine.find().count() === 0) {
+  MusicMachine.insert({slide: 50, startdac: 0, drums: 1, bassline: 1, arp: 1, cymbal: 1, snaredrum: 1, hi_hat: 1});
+}
+ 
